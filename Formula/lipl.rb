12c0db93c93cb2b712089c57ac41a9137b4fbbc2
Robin Guignard-Perret <yxdunc@users.noreply.github.com>
2020-12-16 17:27:57 +0100

    Increment lipl to v0.1.3

--- Formula/lipl.rb
+++ Formula/lipl.rb
@@ -1,13 +1,13 @@
 class Lipl < Formula
-  version "v0.1.2"
+  version "v0.1.3"
   desc "An extended alternative to unix command 'watch' written in Rust"
   homepage "https://github.com/yxdunc/lipl"
   if OS.mac?
     url "https://github.com/yxdunc/lipl/releases/download/#{version}/lipl-osx.tar.gz"
-    sha256 "3532f6df20dc3913f5ded476c13fcd5b719f3b66fbb41c24c173d451a02f68d7"
+    sha256 "07badf1a6061b2374b05359398ae654809a4dac0e69ab8019604ce9442ab86de"
   elsif OS.linux?
     url "https://github.com/yxdunc/lipl/releases/download/#{version}/lipl-linux.tar.gz"
-    sha256 "55f86c30e7797818195e5003196e9e83d7ea4b1bc2b9545cb5fd02444782c239"
+    sha256 "32f22e39813846566c4a0f9f1e009957a53b1c456823e5dbdffca0de7f6f701f"
   end
 
 
